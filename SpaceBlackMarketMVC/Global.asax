7726b16455a3c9216614b4f380ae26af1e14703a
Peter-Nikolov1 <nikolov_peter@yahoo.com>
2022-01-29 12:01:32 -0500

    Initial Commit

---
+++ SpaceBlackMarketMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SpaceBlackMarketMVC.MvcApplication" Language="C#" %>
---
+++ SpaceBlackMarketMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SpaceBlackMarketMVC.Startup))]
+namespace SpaceBlackMarketMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
