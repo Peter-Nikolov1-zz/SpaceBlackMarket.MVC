a8fd0802cce0e11d2c3c75c010ae82f9ffcac39b
Peter-Nikolov1 <nikolov_peter@yahoo.com>
2022-02-06 20:03:31 -0500

    Roles Migration

---
+++ SpaceBlackMarket.Data/Migrations/202202070055441_RolesCreated.cs
@@ -0,0 +1,16 @@
+namespace SpaceBlackMarket.Data.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class RolesCreated : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
